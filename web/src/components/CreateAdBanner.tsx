f0f3143661ba3c10988978216bd612ddc322ec5a
Gean Lucas <gean.lfpsa@gmail.com>
2022-09-26 14:06:11 -0300

    feat: finish web version

--- web/src/components/CreateAdBanner.tsx
+++ web/src/components/CreateAdBanner.tsx
@@ -6,7 +6,7 @@ export default function CreateAdBanner() {
     <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8">
       <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
         <div>
-          <strong className="text-2xl text-white font-black block">Não encntrou seu duo?</strong>
+          <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
           <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
         </div>
         <Dialog.Trigger className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
